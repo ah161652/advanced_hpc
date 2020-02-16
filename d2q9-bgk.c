b5a12a028fd35560a3f202e909a2affde0a689e9
ah161652 <aidan>
2020-02-16 15:44:42 +0000

    ;

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -118,7 +118,7 @@ int main(int argc, char* argv[])
   printf("Elapsed time:\t\t\t%.6lf (s)\n", toc - tic);
   printf("Elapsed user CPU time:\t\t%.6lf (s)\n", usrtim);
   printf("Elapsed system CPU time:\t%.6lf (s)\n", systim);
-  write_values(params, cells, obstacles, av_vels);
+  write_values(params, &cells, obstacles, av_vels);
   finalise(&params, &cells, &tmp_cells, &obstacles, &av_vels);
 
   return EXIT_SUCCESS;
