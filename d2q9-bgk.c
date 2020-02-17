7f9bd509554f9f184d799d2dc808eb3c778aeb17
ah161652 <aidan>
2020-02-17 15:40:42 +0000

    pragma around init

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -426,7 +426,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
   float w1 = params->density      / 9.f;
   float w2 = params->density      / 36.f;
 
-//#pragma omp parallel for schedule(static)
+  #pragma omp parallel for schedule(static)
   for (int jj = 0; jj < params->ny; jj++)
   {
     for (int ii = 0; ii < params->nx; ii++)
