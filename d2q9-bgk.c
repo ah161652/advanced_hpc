44f99e35bf153b54859f540988cf4139b07b5e7e
ah161652 <aidan>
2020-02-20 21:20:46 +0000

    moved __assumed

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -204,7 +204,25 @@ float fusion(const t_param params, t_speed* restrict cells, t_speed* restrict tm
     }
   }
 
+  __assume_aligned(cells->speeds0, 64);
+  __assume_aligned(cells->speeds1, 64);
+  __assume_aligned(cells->speeds2, 64);
+  __assume_aligned(cells->speeds3, 64);
+  __assume_aligned(cells->speeds4, 64);
+  __assume_aligned(cells->speeds5, 64);
+  __assume_aligned(cells->speeds6, 64);
+  __assume_aligned(cells->speeds7, 64);
+  __assume_aligned(cells->speeds8, 64);
 
+  __assume_aligned(tmp_cells->speeds0, 64);
+  __assume_aligned(tmp_cells->speeds1, 64);
+  __assume_aligned(tmp_cells->speeds2, 64);
+  __assume_aligned(tmp_cells->speeds3, 64);
+  __assume_aligned(tmp_cells->speeds4, 64);
+  __assume_aligned(tmp_cells->speeds5, 64);
+  __assume_aligned(tmp_cells->speeds6, 64);
+  __assume_aligned(tmp_cells->speeds7, 64);
+  __assume_aligned(tmp_cells->speeds8, 64);
 
   #pragma omp for nowait schedule(static)
   // #pragma simd aligned
@@ -216,25 +234,7 @@ float fusion(const t_param params, t_speed* restrict cells, t_speed* restrict tm
     #pragma simd
     for (int ii = 0; ii < params.nx; ii++)
     {
-      __assume_aligned(cells->speeds0, 64);
-      __assume_aligned(cells->speeds1, 64);
-      __assume_aligned(cells->speeds2, 64);
-      __assume_aligned(cells->speeds3, 64);
-      __assume_aligned(cells->speeds4, 64);
-      __assume_aligned(cells->speeds5, 64);
-      __assume_aligned(cells->speeds6, 64);
-      __assume_aligned(cells->speeds7, 64);
-      __assume_aligned(cells->speeds8, 64);
 
-      __assume_aligned(tmp_cells->speeds0, 64);
-      __assume_aligned(tmp_cells->speeds1, 64);
-      __assume_aligned(tmp_cells->speeds2, 64);
-      __assume_aligned(tmp_cells->speeds3, 64);
-      __assume_aligned(tmp_cells->speeds4, 64);
-      __assume_aligned(tmp_cells->speeds5, 64);
-      __assume_aligned(tmp_cells->speeds6, 64);
-      __assume_aligned(tmp_cells->speeds7, 64);
-      __assume_aligned(tmp_cells->speeds8, 64);
 
 
 
