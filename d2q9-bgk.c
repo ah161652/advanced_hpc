7c0224ba642c5a8f98abf78f3749121d28dddd35
ah161652 <aidan>
2020-02-20 15:16:33 +0000

    OMP just on fusion

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -458,10 +458,10 @@ int initialise(const char* paramfile, const char* obstaclefile,
   float w1 = params->density      / 9.f;
   float w2 = params->density      / 36.f;
 
- #pragma omp parallel num_threads(8)
-{
-
-#pragma omp for nowait schedule(guided)
+//  #pragma omp parallel num_threads(8)
+// {
+//
+// #pragma omp for nowait schedule(guided)
   for (int jj = 0; jj < params->ny; jj++)
   {
     for (int ii = 0; ii < params->nx; ii++)
@@ -490,7 +490,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
       (*obstacles_ptr)[ii + jj*params->nx] = 0;
     }
   }
- }
+ // }
 
 
 
