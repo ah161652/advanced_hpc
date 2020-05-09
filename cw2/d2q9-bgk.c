41f365c4c73355dbddab3269ec610c4c44687541
ah161652 <aidan>
2020-05-09 12:20:59 +0100

    16x16

--- cw2/d2q9-bgk.c
+++ cw2/d2q9-bgk.c
@@ -233,7 +233,7 @@ float fusion1(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obs
 
     //set global and local sizes
     size_t global[2] = {params.nx, params.ny};
-    size_t local[2] = {32,32};
+    size_t local[2] = {16,16};
 
 
 
@@ -324,7 +324,7 @@ float fusion2(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obs
 
       //set global and local sizes
       size_t global[2] = {params.nx, params.ny};
-      size_t local[2] = {32, 32};
+      size_t local[2] = {16, 16};
 
 
 
@@ -639,7 +639,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
   // err = clGetKernelWorkGroupInfo (ocl->fusion1, ocl->device, CL_KERNEL_WORK_GROUP_SIZE, sizeof(size_t), &work_group_size, NULL);
   // checkError(err, "Getting kernel work group info", __LINE__);
 
-  work_group_size =32*32;
+  work_group_size =16*16;
 
   nwork_groups = (params->nx * params->ny)/work_group_size;
 
